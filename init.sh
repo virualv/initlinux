38b075712c3871a9181aedc11774f8351bc09705
virualv <virualv@gmail.com>
2022-04-05 11:30:03 +0800

    update repo option

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+sudo apt update
 sudo apt-get -y install build-essential asciidoc binutils bzip2 git \
     libz-dev patch python3 python2.7 unzip zlib1g-dev \
     git-core gcc-multilib p7zip p7zip-full libssl-dev \
