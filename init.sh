941a5c624a72f7837f81e9db977a849cf5f56491
virualv <42433288+virualv@users.noreply.github.com>
2022-04-05 11:39:33 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt-get -y install build-essential asciidoc binutils bzip2 git \
     libz-dev patch python3 python2.7 unzip zlib1g-dev \
-    git-core gcc-multilib p7zip p7zip-full libssl-dev \
+    p7zip p7zip-full libssl-dev \
     wget curl rsync \
     tmux aria2 tig htop
 
