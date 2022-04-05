020b34a605c5f1115634f0825d65bd2a30728051
virualv <virualv@gmail.com>
2022-04-05 11:30:03 +0800

    update repo option

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+sudo apt update
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git \
     libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 \
     subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo \
